bd75ff8dd9073f0d3c50ed823bbadfcd5715c099
mous1985 <mustapha.benazzouz@outlook.fr>
2025-03-09 22:08:51 +0100

    ref: refactor code for more performance docs: update docs in EN

--- functions/functions.go
+++ functions/functions.go
@@ -8,117 +8,90 @@ import (
 	"strings"
 )
 
-// NbrOfAntsInRoom find the number of ants in the room
-// and return the number of ants and the data of the room
+// NbrOfAntsInRoom finds the number of ants in the room
+// and returns the number of ants and the data of the room
 func NbrOfAntsInRoom(data []string) (int, []string) {
-	var (
-		nbrAnts  int
-		dataRoom []string
-		err      error
-	)
-
-	for i := 0; i < len(data); i++ {
-		if data[i][0] != '#' {
-			nbrAnts, err = strconv.Atoi(data[i])
-			if nbrAnts < 1 {
-				err = errors.New("EROOR:Not enough ants")
+	for i, line := range data {
+		if line[0] != '#' {
+			nbrAnts, err := strconv.Atoi(line)
+			if err != nil || nbrAnts < 1 {
+				ErrorCheck(errors.New("ERROR: Invalid number of ants"))
 			}
-			ErrorCheck(err)
-			dataRoom = data[i+1:]
-			break
+			return nbrAnts, data[i+1:]
 		}
 	}
-
-	return nbrAnts, dataRoom
+	ErrorCheck(errors.New("ERROR: No valid number of ants found"))
+	return 0, nil
 }
 
+// SeparData separates room data into two categories: room information and relationships.
 func SeparData(roomData []string) ([]string, []string) {
-	var dataEmplace []string
+	var dataEmplace, relationData []string
 
-	// sparé les coordonées de la room
-	for i := 0; i < len(roomData); i++ {
-		if len(strings.Split(roomData[i], " ")) == 3 {
-			dataEmplace = append(dataEmplace, roomData[i])
-		} else if roomData[i][0] == '#' {
-			if roomData[i][1] == '#' {
-				dataEmplace = append(dataEmplace, roomData[i])
-			}
+	for _, line := range roomData {
+		if len(strings.Fields(line)) == 3 || strings.HasPrefix(line, "##") {
+			dataEmplace = append(dataEmplace, line)
+		} else if line[0] == '#' {
+			continue
+		} else if strings.Count(line, "-") == 1 {
+			relationData = append(relationData, line)
 		} else {
-			roomData = roomData[i:]
-			break
+			ErrorCheck(errors.New("ERROR: Invalid data format"))
 		}
 	}
 
-	// les relations entre les rooms
-	var realationData []string
-
-	for _, v := range roomData {
-		if len(strings.Split(v, "-")) == 2 {
-			realationData = append(realationData, v)
-		} else if v[0] == '#' && v[1] != '#' {
-		} else {
-			var err error = nil
-			err = errors.New("ERROR:invalid data format")
-			ErrorCheck(err)
-		}
-	}
-
-	return dataEmplace, realationData
+	return dataEmplace, relationData
 }
 
-// la Map des room et leurs coordonnées
+// CoordoneMapRoom creates a map of rooms and their coordinates
 func CoordoneMapRoom(lignes []string) map[string][]int {
 	roomMap := make(map[string][]int)
-	var err error
+	var startPoint, endPoint int
+	var startPointer, endPointer *int
 
-	// check pour la room ##start et ##end emplacement
-	var startPoint int
-	var endPoint int
-	var startPointer *int
-	var endPointer *int
-
-	for i, v := range lignes {
-		if v == "##start" {
+	for i, line := range lignes {
+		switch line {
+		case "##start":
 			startPoint = i + 1
 			startPointer = &startPoint
-		} else if v == "##end" {
+		case "##end":
 			endPoint = i + 1
 			endPointer = &endPoint
 		}
+		if startPointer != nil && endPointer != nil {
+			break
+		}
 	}
-	// si l'une des lignes ##start ou ##end n'existe pas, affiche error
+
 	if startPointer == nil || endPointer == nil {
-		err = errors.New("ERROR:il manque la room start ou la room end")
-		ErrorCheck(err)
+		ErrorCheck(errors.New("ERROR: Missing start or end room"))
+		return nil
 	}
 
-	// met chaque emplacement et ses coordonnées dans la roomMap
-
-	for i := 0; i < len(lignes); i++ {
-		room := strings.Split(lignes[i], " ")
+	for i := startPoint; i <= endPoint; i++ {
+		if strings.HasPrefix(lignes[i], "##") {
+			continue
+		}
+		room := strings.Fields(lignes[i])
 		if len(room) == 3 {
 			coorX, err := strconv.Atoi(room[1])
 			ErrorCheck(err)
 			coorY, err := strconv.Atoi(room[2])
 			ErrorCheck(err)
+			position := 1
 			if i == startPoint {
-				roomMap[room[0]] = append(roomMap[room[0]], 0, coorX, coorY)
+				position = 0
 			} else if i == endPoint {
-				roomMap[room[0]] = append(roomMap[room[0]], 2, coorX, coorY)
-			} else if lignes[i][:2] != "##" {
-				roomMap[room[0]] = append(roomMap[room[0]], 1, coorX, coorY)
+				position = 2
 			}
+			roomMap[room[0]] = []int{position, coorX, coorY}
 		}
 	}
 
-	// vérification des salles avec les meme coordonnées
 	for key1, v1 := range roomMap {
 		for key2, v2 := range roomMap {
-			if key1 == key2 {
-				break
-			} else if v1[1] == v2[1] && v1[2] == v2[2] {
-				err := errors.New(key1 + " et " + key2 + " ont les meme coordonnées ")
-				ErrorCheck(err)
+			if key1 != key2 && v1[1] == v2[1] && v1[2] == v2[2] {
+				ErrorCheck(errors.New(key1 + " and " + key2 + " have the same coordinates"))
 			}
 		}
 	}
@@ -126,9 +99,8 @@ func CoordoneMapRoom(lignes []string) map[string][]int {
 	return roomMap
 }
 
-// la map des connexions entre les pièces
+// MapRoomConnections creates a map of connections between rooms
 func MapRoomConnections(dataBrute []string, coordoneMap map[string][]int) map[string][]string {
-	var err error
 	originalMap := make(map[string][]string)
 
 	for key, v := range coordoneMap {
@@ -138,62 +110,57 @@ func MapRoomConnections(dataBrute []string, coordoneMap map[string][]int) map[st
 		case 2:
 			originalMap[key] = append(originalMap[key], "end")
 		default:
-			originalMap[key] = append(originalMap[key], "milieu")
+			originalMap[key] = append(originalMap[key], "middle")
 		}
 	}
 
-	// ajouter les relations a la map
 	for _, v := range dataBrute {
 		connection := strings.Split(v, "-")
-		if originalMap[connection[0]] != nil && originalMap[connection[1]] != nil {
-			originalMap[connection[0]] = append(originalMap[connection[0]], connection[1])
-			originalMap[connection[1]] = append(originalMap[connection[1]], connection[0])
+		if len(connection) == 2 {
+			if _, ok := originalMap[connection[0]]; ok {
+				originalMap[connection[0]] = append(originalMap[connection[0]], connection[1])
+			}
+			if _, ok := originalMap[connection[1]]; ok {
+				originalMap[connection[1]] = append(originalMap[connection[1]], connection[0])
+			}
 		} else {
-			err = errors.New("ERROR:manque emplacement")
-			ErrorCheck(err)
+			ErrorCheck(errors.New("ERROR: Invalid connection format"))
 		}
 	}
+
 	return originalMap
 }
 
-// supprimé les chemains sans issus
+// SupprSsissu removes dead-end paths
 func SupprSsissu(mapDepart map[string][]string) map[string][]string {
-	var err error
-
 	for key, value := range mapDepart {
-		if len(value) == 1 {
-			if value[0] == "start" || value[0] == "end" {
-				err = errors.New("ERROR:il manque un chemain ou la salle start")
-				ErrorCheck(err)
-			} else {
-				delete(mapDepart, key)
-			}
+		if len(value) == 1 && value[0] != "start" && value[0] != "end" {
+			delete(mapDepart, key)
 		} else if len(value) == 2 && value[0] != "start" && value[0] != "end" {
 			for key1, values := range mapDepart {
 				for i, v := range values {
 					if v == key {
-						values = Remove(values, i)
-						mapDepart[key1] = values
+						mapDepart[key1] = Remove(values, i)
 					}
 				}
 			}
 			delete(mapDepart, key)
-			SupprSsissu(mapDepart)
+			return SupprSsissu(mapDepart)
 		}
 	}
 
 	return mapDepart
 }
 
-// remove supprime une valeur nommée de la string
+// Remove removes a value from a slice of strings
 func Remove(slice []string, s int) []string {
 	return append(slice[:s], slice[s+1:]...)
 }
 
-// Return  error si err!=nil
+// ErrorCheck prints an error message if err is not nil
 func ErrorCheck(e error) {
 	if e != nil {
-		fmt.Println("ERROR:invalid data format")
+		fmt.Println("ERROR: Invalid data format")
 		log.Fatal(e)
 	}
 }
